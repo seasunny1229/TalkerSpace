d9ba759ff08229bfd977f83e204d62e15a4b75e6
cuiting <sea_sunny1229@163.com>
2020-05-02 15:29:22 +0800

    CHG: log util

--- framework/src/main/java/com/seasunny/framework/util/LogUtil.java
+++ framework/src/main/java/com/seasunny/framework/util/LogUtil.java
@@ -7,28 +7,63 @@ import com.seasunny.framework.BuildConfig;
 
 public class LogUtil {
 
+    public static void d(Class clazz, String text){
+        d(getClassTag(clazz), text);
+    }
+
     public static void d(String tag, String text){
-        if(BuildConfig.LOG_DEBUG){
-            if(!TextUtils.isEmpty(text)){
-                Log.e(tag, text);
-            }
-        }
+        log(Log.DEBUG, tag, text);
+    }
+
+    public static void i(Class clazz, String text){
+        i(getClassTag(clazz), text);
     }
 
     public static void i(String tag, String text){
-        if(BuildConfig.LOG_DEBUG){
-            if(!TextUtils.isEmpty(text)){
-                Log.i(tag, text);
-            }
-        }
+        log(Log.INFO, tag, text);
+    }
+
+    public static void e(Class clazz, String text){
+        e(getClassTag(clazz), text);
     }
 
     public static void e(String tag, String text){
-        if(BuildConfig.LOG_DEBUG){
-            if(!TextUtils.isEmpty(text)){
-                Log.e(tag, text);
-            }
+        log(Log.ERROR, tag, text);
+    }
+
+    private static String getClassTag(Class clazz){
+        return clazz.getSimpleName();
+    }
+
+    private static void log(int priority, String tag, String msg){
+        if(!BuildConfig.LOG_DEBUG){
+            return;
         }
+
+        if(TextUtils.isEmpty(msg)){
+           return;
+        }
+
+        switch (priority){
+            case Log.VERBOSE:
+                Log.v(tag, msg);
+                break;
+            case Log.DEBUG:
+                Log.d(tag, msg);
+                break;
+            case Log.INFO:
+                Log.i(tag, msg);
+                break;
+            case Log.WARN:
+                Log.w(tag, msg);
+                break;
+            case Log.ERROR:
+                Log.e(tag, msg);
+                break;
+            default:
+                break;
+        }
+
     }
 
 }
