4a155ff2326121dd8cdf1ee19421cf4ae905dfc0
cuiting <sea_sunny1229@163.com>
2020-05-10 22:32:09 +0800

    ADD: statistics module

---
+++ statistics/src/main/java/com/seasunny/statistics/channel/Channel.java
@@ -0,0 +1,12 @@
+package com.seasunny.statistics.channel;
+
+public class Channel {
+
+    public static String channel = ChannelName.generic.name();
+
+
+    public enum ChannelName{
+        generic
+    }
+
+}
