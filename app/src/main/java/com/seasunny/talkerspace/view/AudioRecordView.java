aae9730cea8b4d09f42a60288e7b5897f70a513b
cuiting <sea_sunny1229@163.com>
2020-05-17 16:42:50 +0800

    CHG: layout lays

---
+++ app/src/main/java/com/seasunny/talkerspace/view/AudioRecordView.java
@@ -0,0 +1,22 @@
+package com.seasunny.talkerspace.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class AudioRecordView extends View {
+
+    public AudioRecordView(Context context) {
+        super(context);
+    }
+
+    public AudioRecordView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AudioRecordView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/com/seasunny/talkerspace/view/GalleryView.java
@@ -0,0 +1,22 @@
+package com.seasunny.talkerspace.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GalleryView extends View {
+
+    public GalleryView(Context context) {
+        super(context);
+    }
+
+    public GalleryView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GalleryView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
